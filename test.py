9c868d1347a0a91cb9c1925c524e05a56bf0d240
adithya.raveendran <adithya.raveendran@6dtech.co.in>
2025-09-26 13:02:18 +0530

    start loght

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-# n=10
-# for i in range(10,0,-1):
-#     print("*" * i)
+def galactico(n):
+    for i in range(1,n+1):
+        print('*' * i)
 
-n=10
-for i in range(1,n):
-    print("*" * i)
-
-def namer(ch) :
-     print(f"My Name is {ch}")
-
-namer("Mohith")
+x = int(input("Enter the limit : "))
+galactico(x)
