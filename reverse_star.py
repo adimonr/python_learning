1fa44c78d125035272dd8a05bc2a33051677b76c
adithya.raveendran <adithya.raveendran@6dtech.co.in>
2025-10-03 12:37:36 +0530

    tets

--- reverse_star.py
+++ reverse_star.py
@@ -1,9 +1,19 @@
-limit=int(input("Enter the limit :"))
-
-
 def rever_star(i):
     for i in range (i,0,-1):
         print('*' * i)
 
 
-rever_star(limit)
+
+def forward_str(i):
+    for i in range(0,i+1,1):
+        print('*' * i)
+
+limit=int(input("Enter the limit :"))
+ch=int(input("Choices \n1:Ascending Start\n2:Descending Star\nEnter your choice:"))
+if ch==1:
+   forward_str(limit)
+elif ch==2:
+    rever_star(limit)
+else:
+    print("Invalid Input")       
+    #######################
